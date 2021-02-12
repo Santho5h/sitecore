5f52e82dad36a4c6159303b9afc80659db920b52
Santhosh V <55824782+Santho5h@users.noreply.github.com>
2021-02-12 13:28:26 +0530

    Create Exercise-2.sh

---
+++ Exercise-2.sh
@@ -0,0 +1,17 @@
+#install docker service
+sudo apt install docker.io
+sudo usermod -aG docker ${USER}
+
+#installing Docker container
+# --network=elastic_search \
+sudo docker run -d \
+        -p 9200:9200 \
+        -p 9300:9300 \
+        --name elasticsearch \
+        -e "discovery.type=single-node" \
+        docker.elastic.co/elasticsearch/elasticsearch:6.6.1
+#start docker
+sudo docker start /elasticsearch
+
+# health check
+curl -s localhost:9200/_cat/health curl -X GET "localhost:9200/_cat/nodes?v=true&pretty"
